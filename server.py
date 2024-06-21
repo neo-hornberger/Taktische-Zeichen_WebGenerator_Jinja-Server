640c9dc792295ec25aa116fcbc172fca24dca65c
Neo Hornberger <neo.hornberger@gmx.de>
2024-06-21 22:48:29 +0200

    Added symbol library
    - updated submodule 'Taktische-Zeichen'
    - the symbol library contains all predefined symbols

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import urllib3
 import urllib.parse as urlparse
 import json
 
+
 def main():
 	global env
 	env = jinja2.Environment(
@@ -15,6 +16,21 @@ def main():
 
 	host = os.environ.get('SERVER_HOST', 'localhost')
 	port = int(os.environ.get('SERVER_PORT', 9000))
+
+	global symbols
+	symbols = []
+	symbols_dir = './Taktische-Zeichen/symbols/'
+	for path, subdirs, files in os.walk(symbols_dir):
+		dir_name = os.path.relpath(path, symbols_dir)
+		if not dir_name == '.test':
+			for name in files:
+				if name.endswith('.j2'):
+					symbols.append(os.path.join(dir_name, name))
+	symbols.sort()
+	
+	global symbol_themes
+	with open('./Taktische-Zeichen/themes/default.json') as f:
+		symbol_themes = json.load(f)
 
 	print(f'Starting server on {host}:{port}')
 	print()
@@ -42,10 +58,11 @@ class JinjaRequestHandler(http.server.BaseHTTPRequestHandler):
 
 	def do_GET(self):
 		url = urllib3.util.parse_url(self.path)
+		query = urlparse.parse_qs(url.query)
+
+		print(query)
 
 		if url.path == '/build':
-			query = urlparse.parse_qs(url.query)
-
 			template = None
 			if 'template' in query:
 				match query['template'][0]:
@@ -72,7 +89,6 @@ class JinjaRequestHandler(http.server.BaseHTTPRequestHandler):
 
 			try:
 				variables = { key: parseValue(value[0]) for key, value in query.items() }
-				print(variables)
 				body = template.render(variables).encode('utf-8')
 
 				self.send_response(200)
@@ -89,6 +105,36 @@ class JinjaRequestHandler(http.server.BaseHTTPRequestHandler):
 					self.wfile.write(e.message[33:-2].encode('utf-8'))
 				else:
 					self.wfile.write(str(e).encode('utf-8'))
+			
+			return
+		elif url.path == '/library':
+			if 'symbol' in query:
+				symbol = query['symbol'][0]
+				if symbol in symbols:
+					try:
+						body = env.get_template(f'symbols/{symbol}').render(symbol_themes).encode('utf-8')
+
+						self.send_response(200)
+						self._cors_headers()
+						self.send_header('Content-type', 'image/svg+xml')
+						self.end_headers()
+						self.wfile.write(body)
+					except jinja2.exceptions.TemplateRuntimeError as e:
+						self.send_response(400)
+						self.send_header('Content-type', 'text/plain')
+						self._cors_headers()
+						self.end_headers()
+						if isinstance(e, jinja2.exceptions.UndefinedError) and '[ERROR]' in e.message:
+							self.wfile.write(e.message[33:-2].encode('utf-8'))
+						else:
+							self.wfile.write(str(e).encode('utf-8'))
+			else:
+				self.send_response(200)
+				self._cors_headers()
+				self.send_header('Content-type', 'application/json')
+				self.end_headers()
+				self.wfile.write(json.dumps(symbols).encode('utf-8'))
+			
 			return
 
 		self.send_response(400)
